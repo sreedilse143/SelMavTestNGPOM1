767d826558157dcf6b0b09a8f72e828a2fbdebdc
Acer <Acer@DESKTOP-GUV1GTM.lan>
2020-12-15 16:10:15 +0530

    Code to be tested

--- src/test/java/com/amazon/AmazonTest1.java
+++ src/test/java/com/amazon/AmazonTest1.java
@@ -5,21 +5,31 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.amazonpages.AmazonLoginPage;
+import com.amazonpages.OrderProductPage;
 import com.wrapers.BaseTest;
 
 public class AmazonTest1 extends BaseTest {
 
 	@Test(priority = 1)
-	@Parameters(value = {"TD_Username", "TD_password" })	
+	@Parameters(value = { "TD_Username", "TD_password" })
 	public void VerifyAmazonLoginPageTitle() {
 		String pt = page.getInstance(AmazonLoginPage.class).getPageTitle();
-		AssertJUnit.assertEquals(pt, "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in");
+		AssertJUnit.assertEquals(pt,
+				"Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in");
 	}
 
 	@Test(priority = 2)
-	@Parameters(value = {"TD_Username", "TD_password" })	
+	@Parameters(value = { "TD_Username", "TD_password" })
 	public void AmazonLoginTest(String TD_Username, String TD_password) {
 		page.getInstance(AmazonLoginPage.class).login(TD_Username, TD_password);
+	}
+
+	@Test(priority = 3)
+	@Parameters(value = { "TD_Username", "TD_password", "TD_ProdType", "TD_ProdName" })
+	public void ProductAddtoCart(String TD_Username, String TD_password, String TD_ProdType, String TD_ProdName) {
+		page.getInstance(AmazonLoginPage.class).login(TD_Username, TD_password);
+		page.getInstance(OrderProductPage.class).placetocart(TD_ProdType, TD_ProdName);
+
 	}
 
 }
